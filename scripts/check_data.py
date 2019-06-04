586e99493cb016cbc643a13e9fb20c8ab90126c7
jungwook-lee <jungwook@gmail.com>
2019-06-04 13:23:58 -0400

    Finish docs for check_data

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,22 +1,31 @@
+# Script to check class imbalance in the data
+
 import os
 import os.path as path
+
 import numpy as np
 import skimage.io as io
-
 import learn2seg.tools as tools
 
 
-def check_class_balance(label_path, im_size):
+def check_class_balance(label_dir, im_size):
+    """ Checks the class imbalance of given label file images for segmentation
+
+    Args:
+        label_dir (str): path to the label directory
+        im_size (tuple): image size as (height, width)
+
+    """
     total_pixels = im_size[0] * im_size[1]
-    files = sorted(os.listdir(label_path))
+    files = sorted(os.listdir(label_dir))
 
     total_pos = 0
     total_neg = 0
 
     for f_name in files:
-        img = io.imread(path.join(label_path, f_name), as_gray=True)
+        img = io.imread(path.join(label_dir, f_name), as_gray=True)
         img = (img / 255).astype(int)
-        # Get all summation of 0, 1's
+
         pos = np.sum(img)
         total_pos += pos
         total_neg += (total_pixels - pos)
@@ -37,15 +46,15 @@ if __name__ == '__main__':
 
     # Checking class balance for train set
     print("Checking binary balance for train set ...")
-    label_path = path.join(data_path, 'train/label')
-    check_class_balance(label_path, im_size)
+    label_dir = path.join(data_path, 'train/label')
+    check_class_balance(label_dir, im_size)
 
     # Checking class balance for validation set
     print("Checking binary balance for validation set ...")
-    label_path = path.join(data_path, 'val/label')
-    check_class_balance(label_path, im_size)
+    label_dir = path.join(data_path, 'val/label')
+    check_class_balance(label_dir, im_size)
 
     # Checking class balance for test
     print("Checking binary balance for test set ...")
-    label_path = path.join(data_path, 'test/label')
-    check_class_balance(label_path, im_size)
+    label_dir = path.join(data_path, 'test/label')
+    check_class_balance(label_dir, im_size)
